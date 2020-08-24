9ade9a9019f407397476fe20dac93422977848d8
marcusfelipe19 <57795949+marcusfelipe19@users.noreply.github.com>
2020-08-24 12:59:13 -0300

    Add files via upload

---
+++ Asteroids/SpaceShip.lua
@@ -0,0 +1,19 @@
+SpaceShip = Class{}
+
+function SpaceShip:init()
+    self.image = love.graphics.newImage('ship.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    self.x = Virtual_Width / 2 - (self.width / 2)
+    self.y = Virtual_Height / 2- (self.height / 2)
+end
+
+function SpaceShip:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function SpaceShip:render()
+    love.graphics.draw(self.x, self.y)
+end
---
+++ Asteroids/main.lua
@@ -0,0 +1,101 @@
+push = require 'push'
+
+Class = require 'class'
+
+require 'SpaceShip'
+
+Window_Width = 1280
+Window_Height = 720
+
+Virtual_Width = 432
+Virtual_Height = 243
+
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Asteroids')
+
+    math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 16)
+
+    love.graphics.setFont(smallFont)
+
+    --Colocar os sons aqui!
+
+    push:setupScreen(Virtual_Width, Virtual_Height, Window_Width, Window_Height, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+
+    score = 0
+
+    Player = SpaceShip
+
+    gameState = 'start'
+
+end
+
+function love.resize(w,h)
+    push:resize(w, h)
+end
+
+function love.update(dt)
+    --Falta esta parte
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+
+    elseif key == 'enter' or 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        elseif gameState == 'defeated' then
+            score = 0
+            gameState = 'play'
+        end
+    end
+end
+
+function love.draw()
+    
+    push:apply('start')
+
+    love.graphics.setFont(smallFont)
+
+    displayScore()
+
+    if gameState == 'start' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Welcome to Asteroids!', 0, 10, Virtual_Width, 'center')
+        love.graphics.printf('Press Enter to begin!', 0, 20, Virtual_Width, 'center')
+    elseif gameState == 'play' then
+
+    elseif gameState == 'defeated' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('You died', 0, 10, Virtual_Width, 'center')
+        love.graphics.printf('Press Enter to restart!', 0, 20, Virtual_Width, 'center')
+    end
+
+    Player:render()
+
+    displayFPS()
+
+    push:apply('end')
+end
+
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), Virtual_Width - 40, 10)
+end
+
+function displayScore()
+    love.graphics.setFont(scoreFont)
+    love.graphics.print("Points = ", 10, 10)
+    love.graphics.print(tostring(score), 20, 10)
+end
